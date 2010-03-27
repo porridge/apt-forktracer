bdf317f5882bcb1045da9fff4649fc9178831b86
Marcin Owsiany <marcin@owsiany.pl>
2010-03-27 19:02:22 +0000

    Removed some useless pmock imports.
    Removed the 'import pmock' lines in a few modules which imported pmock but
    never used it. Also updated copyright year there.

--- lib/apt_forktracer/tests/test_verbose_policy.py
+++ lib/apt_forktracer/tests/test_verbose_policy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # apt-forktracer - a utility for managing package versions
-# Copyright (C) 2008 Marcin Owsiany <porridge@debian.org>
+# Copyright (C) 2008,2010 Marcin Owsiany <porridge@debian.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-import pmock
 import unittest
 
 from apt_forktracer.policy import VerbosePolicy
