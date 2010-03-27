de2e81925fb2067d518ace6f077bf4fc03f911dc
Marcin Owsiany <marcin@owsiany.pl>
2010-03-27 22:21:03 +0000

    Converted test_config_parser to mox.

--- lib/apt_forktracer/tests/test_config_parser.py
+++ lib/apt_forktracer/tests/test_config_parser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # apt-forktracer - a utility for managing package versions
-# Copyright (C) 2008 Marcin Owsiany <porridge@debian.org>
+# Copyright (C) 2008,2010 Marcin Owsiany <porridge@debian.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,28 +15,34 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-import pmock
+import mox
 import unittest
 
 from apt_forktracer.testlib import test_helper
 from apt_forktracer.config_parser import ConfigParser
+from apt_forktracer.config import Config
 
-class Test_ConfigParser(test_helper.TestCase):
+class Test_ConfigParser(test_helper.MoxTestCase):
 	def setUp(self):
+		super(Test_ConfigParser, self).setUp()
 		self.cp = ConfigParser()
-		self.c = self.mock()
+		self.c = self.mox.CreateMock(Config)
 	def test_loading_empty_file(self):
-		self.c.expects(pmock.never()).method("add")
+		self.mox.ReplayAll()
 		self.assertEquals(self.cp.load([], self.c), [])
 	def test_loading_invalid_syntax_file(self):
 		fake_file = ['\n', 'invalid line\n']
+		self.mox.ReplayAll()
 		self.assertRaisesWithMessageContaining(Exception, 'line 2', self.cp.load, fake_file)
 	def test_loading_invalid_stanza_file(self):
 		fake_file = ['\n', 'package:val\n', 'acCepT-OrigiN: val2\n']
+		self.mox.ReplayAll()
 		self.assertRaisesWithMessageContaining(Exception, 'line 3', self.cp.load, fake_file)
 	def test_loading_valid_file(self):
 		fake_file = ['\n', 'package:val\n', 'acCepT-OrigiN: val2\n', ' track-oriGin : a  spaced val \n', 'track-version:foo']
-		self.c.expects(pmock.once()).add(pmock.functor(lambda stanza: stanza.get('package') == 'val'))
+		self.c.add(mox.Func(lambda stanza: stanza.get('package') == 'val'))
+		self.mox.ReplayAll()
+
 		ret = self.cp.load(fake_file, self.c)
 		self.assertEquals(len(ret), 1)
 		self.assertEquals(ret[0].get('package'), 'val')
