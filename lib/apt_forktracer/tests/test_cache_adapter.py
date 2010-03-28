81046c880f331384e26a1ad84e2dfe2764e8c5b5
Marcin Owsiany <marcin@owsiany.pl>
2010-03-28 12:47:50 +0100

    Converted test_cache_adapter to mox.
    Also deleted now useless pmock_ntimes.py

--- lib/apt_forktracer/tests/test_cache_adapter.py
+++ lib/apt_forktracer/tests/test_cache_adapter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # apt-forktracer - a utility for managing package versions
-# Copyright (C) 2008 Marcin Owsiany <porridge@debian.org>
+# Copyright (C) 2008,2010 Marcin Owsiany <porridge@debian.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,31 +16,37 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 import apt_pkg
-import pmock
+import mox
 import unittest
 
+from apt_forktracer.checker import Checker
+from apt_forktracer.policy import Policy
+from apt_forktracer.reporter import Reporter
+from apt_forktracer.status import Status
+from apt_forktracer.depcache_adapter import DepCacheAdapter
 from apt_forktracer.testlib import test_helper
 from apt_forktracer.apt_pkg_adapter import AptPkgAdapter
 from apt_forktracer.cache_adapter import CacheAdapterFactory
 from apt_forktracer.testlib.fake_cache import FakeCache
 from apt_forktracer.testlib.fake_package import FakePackage
 from apt_forktracer.package_adapter import PackageAdapterFactory
-from apt_forktracer.testlib.pmock_ntimes import NTimesInvocationMatcher
+from apt_forktracer.version_adapter import VersionAdapter
 
-class Test_Base_Cache_Adapter(test_helper.TestCase):
+class Test_Base_Cache_Adapter(test_helper.MoxTestCase):
 	def setUp(self):
+		super(Test_Base_Cache_Adapter, self).setUp()
 		self.set_up_fake_cache()
-		self.mock_policy = self.mock()
-		self.mock_reporter = self.mock()
-		self.mock_depcache_adapter = self.mock()
+		self.mock_policy = self.mox.CreateMock(Policy)
+		self.mock_reporter = self.mox.CreateMock(Reporter)
+		self.mock_depcache_adapter = self.mox.CreateMock(DepCacheAdapter)
 		self.package_adapter_factory = PackageAdapterFactory(self.mock_depcache_adapter)
 		self.set_up_apt_pkg()
 		self.apt_pkg_adapter = AptPkgAdapter(self.fake_apt_pkg_module)
 		self.ca = CacheAdapterFactory().create_cache_adapter(self.fake_cache, self.apt_pkg_adapter, self.mock_reporter)
-		self.mock_checker = self.mock()
-		self.version_adapter = self.mock()
+		self.mock_checker = self.mox.CreateMock(Checker)
+		self.version_adapter = self.mox.CreateMock(VersionAdapter)
 		self.version_adapter.string = '1.2.3'
-		self.mock_status = self.mock()
+		self.mock_status = self.mox.CreateMock(Status)
 	def set_up_fake_cache(self):
 		self.fake_cache = FakeCache()
 		self.set_up_fake_cache_tweak()
@@ -50,8 +56,10 @@ class Test_Base_Cache_Adapter(test_helper.TestCase):
 		self.fake_apt_pkg_module = self._create_mock_apt_pkg_module()
 		test_helper.copy_state_constants(self.fake_apt_pkg_module, apt_pkg)
 	def test_basic_stringification_works(self):
+		self.mox.ReplayAll()
 		self.assertContains(str(self.ca), 'CacheAdapter')
 	def test_states_copied(self):
+		self.mox.ReplayAll()
 		self.assertEquals(self.ca.states_we_check[0], apt_pkg.CurStateInstalled)
 		self.assertEquals(self.ca.states_we_check[1], apt_pkg.CurStateHalfConfigured)
 		self.assertEquals(self.ca.states_we_check[2], apt_pkg.CurStateHalfInstalled)
@@ -65,21 +73,25 @@ class Test_Cache_Adapter_With_Old_Apt_Pkg_Class(Test_Base_Cache_Adapter):
 
 class Test_Empty_Cache_Adapter(Test_Base_Cache_Adapter):
 	def test_stringification_with_empty_cache(self):
+		self.mox.ReplayAll()
 		self.assertContains(str(self.ca), '0 package(s)')
 	def test_invokes_checker_zero_times_with_empty_cache(self):
-		self.mock_checker.expects(pmock.never()).check()
+		self.mox.ReplayAll()
 		self.ca.run(self.mock_checker, self.mock_policy, self.package_adapter_factory)
 
 class Test_One_Installed_Package_Cache_Adapter(Test_Base_Cache_Adapter):
 	def set_up_fake_cache_tweak(self):
 		self.fake_cache.append_package(FakePackage())
 	def test_stringification_with_one_package(self):
+		self.mox.ReplayAll()
 		self.assertContains(str(self.ca), '1 package(s)')
 	def test_invokes_checker_one_time_with_cache_containing_one_package(self):
-		self.mock_depcache_adapter.expects(pmock.once()).get_candidate_version(pmock.functor(lambda o: o.name == 'afake')).will(pmock.return_value(self.version_adapter))
-		self.mock_checker.expects(pmock.once()).method("check").will(pmock.return_value(self.mock_status))
-		self.mock_policy.expects(pmock.once()).should_report(pmock.eq(self.mock_status)).will(pmock.return_value(True))
-		self.mock_reporter.expects(pmock.once()).report(pmock.eq(self.mock_status))
+		self.mock_depcache_adapter.get_candidate_version(mox.Func(lambda o: o.name == 'afake')).AndReturn(self.version_adapter)
+		self.mock_checker.check(mox.IgnoreArg()).AndReturn(self.mock_status)
+		self.mock_policy.should_report(self.mock_status).AndReturn(True)
+		self.mock_reporter.report(self.mock_status)
+		self.mox.ReplayAll()
+
 		self.ca.run(self.mock_checker, self.mock_policy, self.package_adapter_factory)
 
 class Test_Two_Installed_Packages_Cache_Adapter(Test_Base_Cache_Adapter):
@@ -87,53 +99,60 @@ class Test_Two_Installed_Packages_Cache_Adapter(Test_Base_Cache_Adapter):
 		self.fake_cache.append_package(FakePackage(name = 'foo'))
 		self.fake_cache.append_package(FakePackage(name = 'foo'))
 	def test_invokes_checker_two_times_with_cache_containing_one_package(self):
-		self.mock_depcache_adapter.expects(NTimesInvocationMatcher(2)).get_candidate_version(pmock.functor(lambda o: o.name == 'foo')).will(pmock.return_value(self.version_adapter))
-		self.mock_checker.expects(NTimesInvocationMatcher(2)).method("check").will(pmock.return_value(self.mock_status))
-		self.mock_policy.expects(NTimesInvocationMatcher(2)).should_report(pmock.eq(self.mock_status)).will(pmock.return_value(True))
-		self.mock_reporter.expects(NTimesInvocationMatcher(2)).report(pmock.eq(self.mock_status))
+		self.mock_depcache_adapter.get_candidate_version(mox.Func(lambda o: o.name == 'foo')).AndReturn(self.version_adapter)
+		self.mock_depcache_adapter.get_candidate_version(mox.Func(lambda o: o.name == 'foo')).AndReturn(self.version_adapter)
+		self.mock_checker.check(mox.IgnoreArg()).AndReturn(self.mock_status)
+		self.mock_checker.check(mox.IgnoreArg()).AndReturn(self.mock_status)
+		self.mock_policy.should_report(self.mock_status).AndReturn(True)
+		self.mock_policy.should_report(self.mock_status).AndReturn(True)
+		self.mock_reporter.report(self.mock_status)
+		self.mock_reporter.report(self.mock_status)
+		self.mox.ReplayAll()
+
 		self.ca.run(self.mock_checker, self.mock_policy, self.package_adapter_factory)
 
 class Test_One_Not_Installed_Package_Cache_Adapter(Test_Base_Cache_Adapter):
 	def set_up_fake_cache_tweak(self):
 		self.fake_cache.append_package(FakePackage(current_state = apt_pkg.CurStateNotInstalled))
 	def test_invokes_checker_zero_times_with_cache_containing_one_not_installed_package(self):
-		self.mock_checker.expects(pmock.never()).method("check")
+		self.mox.ReplayAll()
 		self.ca.run(self.mock_checker, self.mock_policy, self.package_adapter_factory)
 
 class Test_One_Conffiles_Package_Cache_Adapter(Test_Base_Cache_Adapter):
 	def set_up_fake_cache_tweak(self):
 		self.fake_cache.append_package(FakePackage(current_state = apt_pkg.CurStateConfigFiles))
 	def test_invokes_checker_zero_times_with_cache_containing_one_package_with_just_config_files(self):
-		self.mock_checker.expects(pmock.never()).method("check")
+		self.mox.ReplayAll()
 		self.ca.run(self.mock_checker, self.mock_policy, self.package_adapter_factory)
 
 class Test_One_Half_Configured_Package_Cache_Adapter(Test_Base_Cache_Adapter):
 	def set_up_fake_cache_tweak(self):
 		self.fake_cache.append_package(FakePackage(current_state = apt_pkg.CurStateHalfConfigured))
 	def test_invokes_checker_one_time_with_cache_containing_one_package_half_configured(self):
-		self.mock_depcache_adapter.expects(pmock.once()).get_candidate_version(pmock.functor(lambda o: o.name == 'afake')).will(pmock.return_value(self.version_adapter))
-		self.mock_checker.expects(pmock.once()).method("check").will(pmock.return_value(self.mock_status))
-		self.mock_policy.expects(pmock.once()).should_report(pmock.eq(self.mock_status)).will(pmock.return_value(True))
-		self.mock_reporter.expects(pmock.once()).report(pmock.eq(self.mock_status))
+		self.mock_depcache_adapter.get_candidate_version(mox.Func(lambda o: o.name == 'afake')).AndReturn(self.version_adapter)
+		self.mock_checker.check(mox.IgnoreArg()).AndReturn(self.mock_status)
+		self.mock_policy.should_report(self.mock_status).AndReturn(True)
+		self.mock_reporter.report(self.mock_status)
+		self.mox.ReplayAll()
 		self.ca.run(self.mock_checker, self.mock_policy, self.package_adapter_factory)
 
 class Test_One_Half_Installed_Package_Cache_Adapter(Test_Base_Cache_Adapter):
 	def set_up_fake_cache_tweak(self):
 		self.fake_cache.append_package(FakePackage(current_state = apt_pkg.CurStateHalfInstalled))
 	def test_invokes_checker_one_time_with_cache_containing_one_package_half_installed(self):
-		self.mock_depcache_adapter.expects(pmock.once()).get_candidate_version(pmock.functor(lambda o: o.name == 'afake')).will(pmock.return_value(self.version_adapter))
-		self.mock_checker.expects(pmock.once()).method("check").will(pmock.return_value(None))
-		self.mock_policy.expects(pmock.never()).method("should_report")
+		self.mock_depcache_adapter.get_candidate_version(mox.Func(lambda o: o.name == 'afake')).AndReturn(self.version_adapter)
+		self.mock_checker.check(mox.IgnoreArg()).AndReturn(None)
+		self.mox.ReplayAll()
 		self.ca.run(self.mock_checker, self.mock_policy, self.package_adapter_factory)
 
 class Test_One_Unpacked_Package_Cache_Adapter(Test_Base_Cache_Adapter):
 	def set_up_fake_cache_tweak(self):
 		self.fake_cache.append_package(FakePackage(current_state = apt_pkg.CurStateUnPacked))
 	def test_invokes_checker_one_time_with_cache_containing_one_package_half_configured(self):
-		self.mock_depcache_adapter.expects(pmock.once()).get_candidate_version(pmock.functor(lambda o: o.name == 'afake')).will(pmock.return_value(self.version_adapter))
-		self.mock_checker.expects(pmock.once()).check(pmock.functor(lambda o:o.name == 'afake' and o.candidate_version.string == '1.2.3')).will(pmock.return_value(self.mock_status))
-		self.mock_policy.expects(pmock.once()).should_report(pmock.eq(self.mock_status)).will(pmock.return_value(False))
-		self.mock_reporter.expects(pmock.never()).method("report")
+		self.mock_depcache_adapter.get_candidate_version(mox.Func(lambda o: o.name == 'afake')).AndReturn(self.version_adapter)
+		self.mock_checker.check(mox.Func(lambda o:o.name == 'afake' and o.candidate_version.string == '1.2.3')).AndReturn(self.mock_status)
+		self.mock_policy.should_report(self.mock_status).AndReturn(False)
+		self.mox.ReplayAll()
 		self.ca.run(self.mock_checker, self.mock_policy, self.package_adapter_factory)
 
 if __name__ == '__main__':
