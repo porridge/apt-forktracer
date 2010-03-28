40b816248d07106904957930940e8cdc4a3fbb9a
Marcin Owsiany <marcin@owsiany.pl>
2010-03-28 16:28:28 +0100

    Updated copyright year.

--- lib/apt_forktracer/tests/test_config.py
+++ lib/apt_forktracer/tests/test_config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # apt-forktracer - a utility for managing package versions
-# Copyright (C) 2008 Marcin Owsiany <porridge@debian.org>
+# Copyright (C) 2008,2010 Marcin Owsiany <porridge@debian.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
