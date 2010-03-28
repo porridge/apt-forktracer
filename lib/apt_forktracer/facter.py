c269fa040a96e10d215c65822e9187db3c04cb8f
Marcin Owsiany <marcin@owsiany.pl>
2010-03-28 18:34:54 +0100

    More copyright year updates.

--- lib/apt_forktracer/facter.py
+++ lib/apt_forktracer/facter.py
@@ -1,5 +1,5 @@
 # apt-forktracer - a utility for managing package versions
-# Copyright (C) 2008 Marcin Owsiany <porridge@debian.org>
+# Copyright (C) 2008-2010 Marcin Owsiany <porridge@debian.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
